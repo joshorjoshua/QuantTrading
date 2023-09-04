0332d24c1707cd0d8e5e69a818c44811af9fbea4
joshorjoshua <63957814+joshorjoshua@users.noreply.github.com>
2023-09-04 16:19:21 +0900

    Add files via upload

--- util/time_helper.py
+++ util/time_helper.py
@@ -2,6 +2,10 @@ import time
 from datetime import datetime
 
 
+def get_time():
+    now = datetime.now().time().total_seconds()
+
+
 def check_transaction_open():
     """현재 시간이 장 중인지 확인하는 함수"""
     now = datetime.now()
